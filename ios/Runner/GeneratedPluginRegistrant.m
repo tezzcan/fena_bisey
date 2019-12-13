c23afd64cd1cbe830684dcc62a1cdc2a881191d4
Oğuzhan Tezcan <aslanozi55@gmail.com>
2019-12-13 16:14:41 +0300

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_full_pdf_viewer/FlutterFullPdfViewerPlugin.h>)
+#import <flutter_full_pdf_viewer/FlutterFullPdfViewerPlugin.h>
+#else
+@import flutter_full_pdf_viewer;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterFullPdfViewerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterFullPdfViewerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
